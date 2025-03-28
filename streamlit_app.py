734c0e3d10b58ba06d50a53798d62f47f731eb29
JONE DOS SANTOS SODRÉ CORREIA <jonecorreia@gmail.com>
2025-03-28 18:25:28 -0300

    teste

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,11 @@ import pandas as pd
 import streamlit as st
 
 # Show app title and description.
-st.set_page_config(page_title="Support tickets", page_icon="🎫")
+st.set_page_config(page_title="PASCOM Venda de Tickets", page_icon="🎫")
 st.title("🎫 Support tickets")
 st.write(
     """
-    This app shows how you can build an internal tool in Streamlit. Here, we are 
-    implementing a support ticket workflow. The user can create a ticket, edit 
-    existing tickets, and view some statistics.
+    App simplificado de venda de tickets
     """
 )
 
